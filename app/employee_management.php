6414d412bb43a87c4e590690b2abdb9ea1ca80f2
HHHuser1 <134743133+HHHuser1@users.noreply.github.com>
2023-09-20 03:58:58 -0400

    first commit

---
+++ app/employee_management.php
@@ -0,0 +1,81 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require_once('./_includes/db_connect.php');
+
+// Initialize the $employees array to store employee data
+$employees = array();
+
+// Check if the request contains JSON data
+if ($_SERVER["CONTENT_TYPE"] === "application/json") {
+    $json_data = file_get_contents('php://input');
+    $data = json_decode($json_data, true);
+
+    // Check for specific JSON properties to determine the action
+    if (isset($data["fetchEmployees"])) {
+        // Fetch all employees from the database
+        $result = mysqli_query($link, "SELECT * FROM employees ORDER BY timestamp DESC");
+        while ($row = mysqli_fetch_assoc($result)) {
+            $employees[] = $row;
+        }
+    }
+
+    if (isset($data["addEmployee"])) {
+        // Handle employee addition logic here
+        $first_name = $data["first_name"];
+        $last_name = $data["last_name"];
+        $email = $data["email"];
+        $employee_id = $data["employee_id"];
+        $query = "INSERT INTO employees (first_name, last_name, email, employee_id) VALUES ('$first_name', '$last_name', '$email', '$employee_id')";
+        $insert_result = mysqli_query($link, $query);
+        if (!$insert_result) {
+            $error_message = "Insert failed: " . mysqli_error($link);
+            echo json_encode(['error' => $error_message]);
+            exit; // Terminate the script after sending the error JSON response
+        }
+    }
+
+    if (isset($data["updateEmployee"])) {
+        // Handle employee update logic here
+        $update_employee_id = $data["update_employee_id"];
+        $update_first_name = $data["update_first_name"];
+        $update_last_name = $data["update_last_name"];
+        $update_email = $data["update_email"];
+
+        $check_query = "SELECT * FROM employees WHERE employee_id = '$update_employee_id'";
+        $check_result = mysqli_query($link, $check_query);
+
+        // if (mysqli_num_rows($check_result) == 0) {
+        //     die("Error: Employee with ID $update_employee_id does not exist.");
+        //     // die(json_encode(['error' => $error_message]));
+        // } 
+
+        if (mysqli_num_rows($check_result) == 0) {
+            $error_message = " Employee with ID $update_employee_id does not exist.";
+            header('Content-Type: application/json');
+            echo json_encode(['error' => $error_message]);
+            exit; // Terminate the script after sending the error JSON response
+        } else {
+            $query = "UPDATE employees SET first_name = '$update_first_name', last_name = '$update_last_name', email = '$update_email' WHERE employee_id = '$update_employee_id'";
+            $update_result = mysqli_query($link, $query);
+
+        }
+    }
+
+    if (isset($data["searchEmployee"])) {
+        // Handle employee search by first name or last name
+        $search_name = $data["search_name"];
+        $query = "SELECT * FROM employees WHERE first_name LIKE '%$search_name%' OR last_name LIKE '%$search_name%' ORDER BY timestamp DESC";
+        $search_result = mysqli_query($link, $query);
+        while ($row = mysqli_fetch_assoc($search_result)) {
+            $employees[] = $row;
+        }
+    }
+}
+
+// Send employee data as a JSON response
+header('Content-Type: application/json');
+echo json_encode($employees);
+?>
---
+++ js/employee_management.js
@@ -0,0 +1,154 @@
+
+        
+        // Function to display error messages on the page
+        function displayError(errorMessage) {
+            const errorContainer = document.getElementById("errorContainer");
+            errorContainer.innerHTML = `<p style="color: red;">Error: ${errorMessage}</p>`;
+        }
+        
+        
+        
+        // Function to fetch employee data from the server
+        function fetchEmployees() {
+            fetch("./app/employee_management.php", {
+                method: "POST",
+                body: JSON.stringify({ fetchEmployees: true }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(data => displayData(data))
+            .catch(error => console.error(error));
+        }
+
+        // Function to display employee data on the web page
+        function displayData(data) {
+            const employeeList = document.getElementById("employeeList");
+            employeeList.innerHTML = '';
+
+            const ul = document.createElement('ul');
+            
+            data.forEach((employee) => {
+                const li = document.createElement('li');
+                li.innerHTML = `Name: ${employee.first_name} ${employee.last_name}, Email: ${employee.email}, ID: ${employee.employee_id}`;
+                ul.appendChild(li);
+            })
+
+            employeeList.appendChild(ul);
+        }
+
+        // Call the function to fetch employee data when the page loads
+        fetchEmployees();
+
+        // Function to handle adding an employee
+        // Function to handle adding an employee
+        document.getElementById("addEmployeeForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        const form = event.target;
+        const firstName = form.querySelector("#first_name").value;
+        const lastName = form.querySelector("#last_name").value;
+        const email = form.querySelector("#email").value;
+        const employeeId = form.querySelector("#employee_id").value;
+
+        fetch("./app/employee_management.php", {
+            method: "POST",
+            body: JSON.stringify({ 
+                addEmployee: true,
+                first_name: firstName,
+                last_name: lastName,
+                email: email,
+                employee_id: employeeId
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                // Display the error message on the page
+                displayError(data.error);
+            } else {
+                // Clear the error message and reload the page after adding an employee successfully
+                displayError("");
+                alert("Employee added successfully");
+                location.reload();
+            }
+        })
+        .catch(error => console.error(error));
+});
+
+
+    // Function to display error messages on the page
+    function displayError2(errorMessage) {
+        const errorContainer = document.getElementById("errorContainer2");
+        errorContainer.innerHTML = `<p style="color: red;">Error: ${errorMessage}</p>`;
+    }
+
+    // Function to handle updating an employee
+    // Function to handle updating an employee
+    document.getElementById("updateEmployeeForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    const form = event.target;
+    const updateEmployeeId = form.querySelector("#update_employee_id").value;
+    const updateFirstName = form.querySelector("#update_first_name").value;
+    const updateLastName = form.querySelector("#update_last_name").value;
+    const updateEmail = form.querySelector("#update_email").value;
+
+    fetch("./app/employee_management.php", {
+        method: "POST",
+        body: JSON.stringify({ 
+            updateEmployee: true,
+            update_employee_id: updateEmployeeId,
+            update_first_name: updateFirstName,
+            update_last_name: updateLastName,
+            update_email: updateEmail
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            // Display the error message on the page
+            displayError2(data.error);
+        } else {
+            // Clear the error message and reload the page after updating an employee successfully
+            displayError2("");
+            alert("Employee updated successfully");
+            location.reload();
+        }
+    })
+    .catch(error => console.error(error));
+});
+
+
+
+
+    
+        // Function to handle searching for employees
+        document.getElementById("searchEmployee").addEventListener("click", function() {
+            const searchName = document.getElementById("search_name").value;
+
+            fetch("./app/employee_management.php", {
+                method: "POST",
+                body: JSON.stringify({ 
+                    searchEmployee: true,
+                    search_name: searchName
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+                
+            })
+            .then(response => response.json())
+            .then(data => displayData(data))
+            .catch(error => console.error(error));
+            
+        });
+        
+
+
+        /////
